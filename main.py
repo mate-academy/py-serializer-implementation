fcf5c4dca8eadd5fc5040857665cef8a793a3eb1
Anton Lasko <anton.lasko1993@gmail.com>
2022-08-18 09:16:32 +0300

    'Solution'

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    return JSONRenderer().render(serializer.data)
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    data = JSONParser().parse(json)
+    serializer = CarSerializer(data=data)
+    return serializer.validated_data
