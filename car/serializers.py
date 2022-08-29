80bc4a6a102d65c4bdb66ac425a5726079b7a4b5
Marko <marko@MacBook-Pro-Marko.local>
2022-08-29 17:26:29 +0300

    fixed

--- car/serializers.py
+++ car/serializers.py
@@ -10,7 +10,18 @@ class CarSerializer(serializers.Serializer):
     model = serializers.CharField(max_length=64)
     horse_powers = serializers.IntegerField(validators=[MaxValueValidator(1914), MinValueValidator(1)])
     is_broken = serializers.BooleanField(allow_null=False)
-    problem_description = serializers.CharField(max_length=266, allow_null=True, allow_blank=True)
+    problem_description = serializers.CharField(required=False)
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.manufacturer = validated_data.get("manufacturer", instance.manufacturer)
+        instance.model = validated_data.get("model", instance.model)
+        instance.horse_powers = validated_data.get("horse_powers", instance.horse_powers)
+        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
+        instance.problem_description = validated_data.get("problem_description", instance.problem_description)
+
+        instance.save()
+
+        return instance
