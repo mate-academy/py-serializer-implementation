d713220722ea4acd9b6cb877c8f7c7bce5c58e55
shurushku <nastia.kuchina.741456963@gmail.com>
2022-11-14 16:45:39 +0200

    Add requaired=True to horse_powers

--- car/serializers.py
+++ car/serializers.py
@@ -6,7 +6,9 @@ from .models import Car
 class CarSerializer(serializers.Serializer):
     manufacturer = serializers.CharField(required=True, max_length=64)
     model = serializers.CharField(required=True, max_length=64)
-    horse_powers = serializers.IntegerField(min_value=1, max_value=1914)
+    horse_powers = serializers.IntegerField(
+        required=True, min_value=1, max_value=1914
+    )
     is_broken = serializers.BooleanField(required=True)
     problem_description = serializers.CharField(
         required=False, allow_blank=True
