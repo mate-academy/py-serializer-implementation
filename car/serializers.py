3ef01f7a6ebbf0c54943219c2f39e1e465546078
stoveprofi <stoveprofi@gmail.com>
2022-11-18 11:38:42 +0000

    Corrected conditions for the serializers.py

--- car/serializers.py
+++ car/serializers.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from rest_framework import serializers
 
 from car.models import Car
@@ -5,11 +6,12 @@ from car.models import Car
 
 class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    manufacturer = serializers.CharField()
-    model = serializers.CharField()
-    horse_powers = serializers.IntegerField()
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(
+        validators=[MaxValueValidator(1914), MinValueValidator(1)])
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField()
+    problem_description = serializers.CharField(null=True, blank=True)
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
