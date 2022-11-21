7e732d9f5a9058b5d8fb743e847dc89123d39c82
graandda <graanddadays@gmail.com>
2022-11-21 13:56:54 +0200

    solution2

--- car/serializers.py
+++ car/serializers.py
@@ -5,15 +5,17 @@ from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
     horse_powers = serializers.IntegerField(
         required=True,
-        validators=[MaxValueValidator(1914), MinValueValidator(1)],
+        max_value=1914,
+        min_value=1,
     )
     is_broken = serializers.BooleanField(required=True)
     problem_description = serializers.CharField(
-        required=False, allow_null=True
+        required=False
     )
 
     def create(self, validated_data):
