d7661f2e73416cc4a93d0ca5b361033ee2843158
Danylchuk Maksym <maxitosm00@gmail.com>
2022-12-22 12:11:32 +0200

    ready

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from car.models import Car
 from car.serializers import CarSerializer
-from rest_framework.renderers import JSONRenderer
+# from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 import io
 
 
 def serialize_car_object(car: Car) -> bytes:
-    serializer = CarSerializer(car)
-    return JSONRenderer().render(serializer.data)
+    serializer = CarSerializer(data=car)
+    return serializer.data
 
 
 def deserialize_car_object(json: bytes) -> Car:
