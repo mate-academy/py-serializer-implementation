3a4940559c8ee3bd3e2ebda61e7a617a1ad32fca
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-12-27 20:27:25 -0400

    Complete serializetion and deserialization process

--- car/serializers.py
+++ car/serializers.py
@@ -7,7 +7,7 @@ class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(required=True)
     model = serializers.CharField(required=True)
-    horse_power = serializers.IntegerField(required=True)
+    horse_powers = serializers.IntegerField(required=True)
     is_broken = serializers.BooleanField(required=True)
     problem_description = serializers.CharField(required=False)
 
@@ -18,8 +18,8 @@ class CarSerializer(serializers.Serializer):
         instance.manufacturer = validated_data.get(
             "manufacturer", instance.manufacturer)
         instance.model = validated_data.get("model", instance.model)
-        instance.horse_power = validated_data.get(
-            "horse_power", instance.horse_power)
+        instance.horse_powers = validated_data.get(
+            "horse_power", instance.horse_powers)
         instance.is_broken = validated_data.get(
             "is_broken", instance.is_broken)
         instance.problem_description = validated_data.get(
