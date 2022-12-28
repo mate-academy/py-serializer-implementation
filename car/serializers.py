7d92aca5e7049a40648547468bfe16d60402c72a
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-12-27 20:48:52 -0400

    Change serializer Car

--- car/serializers.py
+++ car/serializers.py
@@ -7,7 +7,8 @@ class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(required=True, max_length=64)
     model = serializers.CharField(required=True, max_length=64)
-    horse_powers = serializers.IntegerField(required=True, max_value=1914, min_value=1)
+    horse_powers = serializers.IntegerField(
+        required=True, max_value=1914, min_value=1)
     is_broken = serializers.BooleanField(required=True)
     problem_description = serializers.CharField(required=False)
 
