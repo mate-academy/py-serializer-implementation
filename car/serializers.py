f6ef88b75327ac6005e68152b70818c35f0d49b2
Vlad Hrytsai <sunrises2929@gmail.com>
2023-01-11 17:56:21 +0200

    solution

--- car/serializers.py
+++ car/serializers.py
@@ -7,6 +7,7 @@ from car.models import Car
 class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64, required=True)
+    model = serializers.CharField(max_length=64, required=True)
     horse_powers = serializers.IntegerField(
         required=True,
         validators=[MaxValueValidator(1914), MinValueValidator(1)]
@@ -20,6 +21,9 @@ class CarSerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.manufacturer = validated_data.get(
             "manufacturer", instance.manufacturer
+        )
+        instance.model = validated_data.get(
+            "model", instance.model
         )
         instance.horse_powers = validated_data.get(
             "horse_powers", instance.horse_powers
