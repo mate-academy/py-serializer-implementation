18c8371ead69b9f31af34de2240e2eade97c495e
Anton Chumak <Chumak7377@gmail.com>
2023-02-02 14:08:01 +0200

    Create CarSerializer

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import io
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    if serializer.is_valid():
+        json = JSONRenderer().render(serializer.data)
+        return json
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    serializer = CarSerializer(json)
+    data = JSONParser().parse(serializer)
+    return data
