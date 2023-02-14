d2fd3fb19fabb7430e34e4926e5c4e01ec24c848
vani@linux <winfkill@gmail.com>
2023-02-14 18:48:42 +0200

    Done

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "car_service.settings")
+django.setup()
+from rest_framework.renderers import JSONRenderer
 from car.models import Car
+from car.serializers import CarSerializer
+import io
+from rest_framework.parsers import JSONParser
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    content = JSONRenderer().render(serializer.data)
+    return content
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    stream = io.BytesIO(json)
+    data = JSONParser().parse(stream)
+    serializer = CarSerializer(data=data)
+    serializer.is_valid()
+    return serializer.save()
