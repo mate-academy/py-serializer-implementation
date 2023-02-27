d3f8203b05863386bfe69ca427f5ef7a63468b60
Mykola Zakharov <kolyazakharov94@gmail.com>
2023-02-27 14:03:06 +0200

    fixed flake8 mistake

--- car/serializers.py
+++ car/serializers.py
@@ -8,7 +8,12 @@ class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
-    horse_powers = serializers.IntegerField(validators=[MaxValueValidator(1914), MinValueValidator(1)])
+    horse_powers = serializers.IntegerField(
+        validators=[
+            MaxValueValidator(1914),
+            MinValueValidator(1)
+        ]
+    )
     is_broken = serializers.BooleanField()
     problem_description = serializers.CharField(required=False)
 
