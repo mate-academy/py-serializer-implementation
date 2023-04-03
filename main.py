a53a0a54104d7c460c58110b7d613c3766b661ad
usrofgh <mailforgimail@gmail.com>
2023-04-03 22:59:16 +0300

    Solution

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+import io
+
+from django.core.exceptions import ValidationError
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    return JSONRenderer().render(CarSerializer(car).data)
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(json_car: bytes) -> Car | ValidationError:
+    car_steam = io.BytesIO(json_car)
+    car_dict = JSONParser().parse(car_steam)
+    car_serializer = CarSerializer(data=car_dict)
+    if car_serializer.is_valid():
+        return Car(**car_dict)
+    return ValidationError("error")
