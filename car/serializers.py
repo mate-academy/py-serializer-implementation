3b7f653bbba9fa965bd0286da478cb69baeb529e
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-04-03 23:03:27 +0200

    Add fields into Car serializer

--- car/serializers.py
+++ car/serializers.py
@@ -6,6 +6,7 @@ from car.models import Car
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
+        fields = "__all__"
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
