9395e417b1d6433a3c4c371edd229827591afcb9
Maksym Nikityuk <maksym71417@gmail.com>
2023-04-04 16:30:54 +0300

    Some Fixes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import io
 
-from rest_framework.exceptions import ValidationError
+from django.core.exceptions import ValidationError
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 
