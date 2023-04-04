104fb75d99a5663db92df4b75d30719fdbd53adb
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-04-04 18:00:46 +0300

    Add validators for horse_powers field of serializer

--- car/serializers.py
+++ car/serializers.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from rest_framework import serializers
 
 from car.models import Car
@@ -5,11 +6,13 @@ from car.models import Car
 
 class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    manufacturer = serializers.CharField(max_length=64)
-    model = serializers.CharField(max_length=64)
-    horse_powers = serializers.IntegerField()
-    is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(allow_null=True)
+    manufacturer = serializers.CharField(max_length=64, required=True)
+    model = serializers.CharField(max_length=64, required=True)
+    horse_powers = serializers.IntegerField(
+        validators=[MaxValueValidator(1914), MinValueValidator(1)]
+    )
+    is_broken = serializers.BooleanField(required=True)
+    problem_description = serializers.CharField(allow_null=True, required=False)
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
