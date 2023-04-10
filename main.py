b93fd2ce30eac1862636d953732f4c453c41c0b4
Yevhen Kopatko <cs.kopatko.evgen@gmail.com>
2023-04-10 17:57:00 +0300

    Solution

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
+from rest_framework.utils import json
 from car.models import Car
 from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
-    return serializer.data
+    json_string = json.dumps(serializer.data)
+    return json_string.encode("utf-8")
 
 
 def deserialize_car_object(json: bytes) -> Car:
