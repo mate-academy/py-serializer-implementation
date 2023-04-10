f0ef59455895f182e9af5c7cdfa39d0f09f4f916
Yevhen Kopatko <cs.kopatko.evgen@gmail.com>
2023-04-10 17:43:41 +0300

    Solution

--- car/serializers.py
+++ car/serializers.py
@@ -9,16 +9,34 @@ class CarSerializer(serializers.Serializer):
     model = serializers.CharField(max_length=64)
     horse_powers = serializers.IntegerField()
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(allow_blank=True, allow_null=True)
+    problem_description = serializers.CharField(
+        allow_blank=True,
+        allow_null=True
+    )
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.manufacturer = validated_data.get("manufacturer", instance.manufacturer)
-        instance.model = validated_data.get("model", instance.model)
-        instance.horse_powers = validated_data.get("horse_powers", instance.horse_powers)
-        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
-        instance.problem_description = validated_data.get("problem_description", instance.problem_description)
+        instance.manufacturer = validated_data.get(
+            "manufacturer",
+            instance.manufacturer
+        )
+        instance.model = validated_data.get(
+            "model",
+            instance.model
+        )
+        instance.horse_powers = validated_data.get(
+            "horse_powers",
+            instance.horse_powers
+        )
+        instance.is_broken = validated_data.get(
+            "is_broken",
+            instance.is_broken
+        )
+        instance.problem_description = validated_data.get(
+            "problem_description",
+            instance.problem_description
+        )
         instance.save()
         return instance
