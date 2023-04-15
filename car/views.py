39930d073c68b8cf45b8bb4c1f486fb56db9b742
Andriy <andriykrav@gmail.com>
2023-04-15 20:44:14 +0300

    Solution with errors while running main.py

--- car/views.py
+++ car/views.py
@@ -1,1 +1,37 @@
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from car.models import Car
+from car.serializers import CarSerializer
+
+
+@api_view(["GET", "POST"])
+def car_list(request):
+    if request.method == "GET":
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == "POST":
+        serializer = CarSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(["GET", "PUT", "DELETE"])
+def car_detail(request, pk):
+    car = Car.objects.get(pk=pk)
+    if request.method == "GET":
+        serializer = CarSerializer(car)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == "PUT":
+        serializer = CarSerializer(car, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    elif request.method == "DELETE":
+        car.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- main.py
+++ main.py
@@ -1,9 +1,36 @@
+import io
+from rest_framework.parsers import JSONParser
+
 from car.models import Car
+from car.serializers import CarSerializer
+from rest_framework.renderers import JSONRenderer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    json_data = JSONRenderer().render(serializer.data)
+    return json_data
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(json_bytes: bytes) -> Car:
+    json_bytes = io.BytesIO(json_bytes)
+    data = JSONParser().parse(json_bytes)
+    serializer = CarSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    return serializer.save()
+
+
+if __name__ == "__main__":
+
+    car = Car.objects.create(
+        manufacturer="Honda",
+        model="Civic",
+        horse_powers=174,
+        is_broken=False,
+        problem_description="",
+    )
+    serialized_car = serialize_car_object(car)
+    print(serialized_car)
+
+    deserialized_car = deserialize_car_object(serialized_car)
+    print(deserialized_car.manufacturer, deserialized_car.model, deserialized_car.horse_powers)
