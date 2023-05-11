2da96033bd78480278e9a868d157f609458dff4a
phaishuk <xomas@ukr.net>
2023-05-11 22:24:33 +0300

    done

--- car/serializers.py
+++ car/serializers.py
@@ -9,7 +9,10 @@ class CarSerializer(serializers.Serializer):
     manufacturer = serializers.CharField(max_length=64, required=True)
     model = serializers.CharField(max_length=64, required=True)
     horse_powers = serializers.IntegerField(
-        validators=[MaxValueValidator(1914), MinValueValidator(1)], required=True
+        validators=[
+            MaxValueValidator(1914), MinValueValidator(1)
+        ],
+        required=True
     )
     is_broken = serializers.BooleanField()
     problem_description = serializers.CharField(
@@ -27,7 +30,9 @@ class CarSerializer(serializers.Serializer):
         instance.horse_powers = validated_data.get(
             "horse_powers", instance.horse_powers
         )
-        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
+        instance.is_broken = validated_data.get(
+            "is_broken", instance.is_broken
+        )
         instance.problem_description = validated_data.get(
             "problem_description", instance.problem_description
         )
