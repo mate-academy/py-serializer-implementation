7304eaa3ca7c32b8aae7560d510bd925487155de
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-07-13 15:56:04 +0300

    Fixed flake8 errors

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from rest_framework.renderers import JSONRenderer
 from car.models import Car
 from car.serializers import CarSerializer
 
+
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
     json = JSONRenderer().render(serializer.data)
