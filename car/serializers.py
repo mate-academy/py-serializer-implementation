74b9a05851c4f2150c9439c96d5b4b0cb5edc131
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-07-28 00:24:01 +0300

    fix

--- car/serializers.py
+++ car/serializers.py
@@ -5,9 +5,9 @@ from car.models import Car
 
 class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    manufacturer = serializers.CharField()
-    model = serializers.CharField()
-    horse_powers = serializers.IntegerField()
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(min_value=1, max_value=1914)
     is_broken = serializers.BooleanField()
     problem_description = serializers.CharField(required=False)
 
