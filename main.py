08a74e8c8e50d0f983ab1b23d28814b3068ddc93
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-07-29 15:43:38 +0300

    Implement the serializer implementation task

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from car.models import Car
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+import io
+
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    return JSONRenderer().render(CarSerializer(car).data)
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    stream = io.BytesIO(json)
+    return Car(**JSONParser().parse(stream))
