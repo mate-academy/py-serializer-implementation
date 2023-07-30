e93e163c1df352dd0443a2ec5fee53b99eb8fc61
Diana Shyrokikh <diashiroks@gmail.com>
2023-07-30 15:39:58 +0300

    Solution

--- car/views.py
+++ car/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from rest_framework import viewsets
+
+from car.models import Car
+from car.serializers import CarSerializer
+
+
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer()
