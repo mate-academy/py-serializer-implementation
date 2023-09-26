f4970a4298c90050e81883f0f6626ec8810d1c7f
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-09-26 20:30:25 +0300

    Solution

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import io
+from rest_framework.exceptions import ValidationError
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
-def serialize_car_object(car: Car) -> bytes:
-    pass
+def serialize_car_object(car: Car) -> JSONRenderer:
+    serializer = CarSerializer(car)
+    return JSONRenderer().render(serializer.data)
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(json: dict) -> Car | ValidationError:
+    stream = io.BytesIO(json)
+    data = JSONParser().parse(stream)
+    serializer = CarSerializer(data=data)
+
+    if serializer.is_valid():
+        return serializer.save()
+    else:
+        return ValidationError(serializer.errors)
