b6e25164786f23daa99fc78236e181a0a041517b
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-11-04 23:39:27 +0200

    added required=False is not required for data entry

--- car/serializers.py
+++ car/serializers.py
@@ -1,14 +1,17 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from rest_framework import serializers
 from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    manufacturer = serializers.CharField()
-    model = serializers.CharField()
-    horse_powers = serializers.IntegerField(min_value=1, max_value=1914)
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(
+        validators=[MaxValueValidator(1914), MinValueValidator(1)]
+    )
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(allow_null=True)
+    problem_description = serializers.CharField(allow_null=True, required=False)
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
