29e61ddd4f289cabbc9655604d63cfc0602fbe5c
Denys <den.prislipskyi@gmail.com>
2023-12-04 13:24:07 +0100

    solution

--- car/serializers.py
+++ car/serializers.py
@@ -2,4 +2,10 @@ from rest_framework import serializers
 
 
 class CarSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(max_value=1914, min_value=1)
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(required=False)
+
