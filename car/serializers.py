786f86d65fe62644c5e2b642d6d0a10c19caa405
Soloveyk1n <swain.volt@gmail.com>
2024-01-12 14:25:26 +0200

    Solution1

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,26 @@
 from rest_framework import serializers
 
+from car.models import Car
 
-class CarSerializer(serializers.Serializer):
-    pass
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = (
+            "manufacturer",
+            "model",
+            "horse_powers",
+            "is_broken",
+            "problem_description"
+        )
+
+    def create(self, validated_data):
+        return Car(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.manufacturer = validated_data.get("manufacturer", instance.manufacturer)
+        instance.model = validated_data.get("model", instance.model)
+        instance.horse_powers = validated_data.get("horse_powers", instance.horse_powers)
+        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
+        instance.problem_description = validated_data.get("problem_description", instance.problem_description)
+        return instance
--- main.py
+++ main.py
@@ -1,9 +1,28 @@
+from rest_framework.parsers import JSONParser
+
 from car.models import Car
+from car.serializers import CarSerializer
+from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+import io
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    return JSONRenderer().render(serializer.data)
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(json: bytes) -> CarSerializer:
+    stream = io.BytesIO(json)
+    data = JSONParser().parse(stream)
+    serializer = CarSerializer(data=data)
+    serializer.is_valid()
+    return serializer.save()
+
+
+print(serialize_car_object(Car.objects.create(
+    manufacturer="Porsche",
+    model="Panamera",
+    horse_powers=1234,
+    is_broken=False,
+)))
