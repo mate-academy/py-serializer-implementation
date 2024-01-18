7c3e8a95479e9fe2154eef58024e16a96c6856c9
Andriy_Shumskyy <andshumskyy@gmail.com>
2024-01-18 16:28:48 +0200

    for check

--- car/serializers.py
+++ car/serializers.py
@@ -4,36 +4,35 @@ from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
-    id = serializers.IntegerField(required=True)
+    id = serializers.IntegerField()
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
     horse_powers = serializers.IntegerField(min_value=100, max_value=300)
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(required=False)
+    problem_description = serializers.CharField(allow_null=True, allow_blank=True, required=False)
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.manufacturer = validated_data(
+        instance.manufacturer = validated_data.get(
             "manufacturer",
             instance.manufacturer
         )
-        instance.model = validated_data(
+        instance.model = validated_data.get(
             "model",
             instance.model
         )
-        instance.horse_powers = validated_data(
+        instance.horse_powers = validated_data.get(
             "horse_powers",
             instance.horse_powers
         )
-        instance.is_broken = validated_data(
+        instance.is_broken = validated_data.get(
             "is_broken",
             instance.is_broken
         )
-        instance.problem_description = validated_data(
-            "problem_description",
-            instance.problem_description
-        )
+
+        instance.problem_description = validated_data.get("problem_description", None)
+
         instance.save()
         return instance
--- main.py
+++ main.py
@@ -16,5 +16,5 @@ def deserialize_car_object(json: bytes) -> Car:
     data = JSONParser().parse(stream)
     serializer = CarSerializer(data=data)
     if serializer.is_valid():
-        serializer.save()
-    return serializer.validated_data
+        car = Car(**serializer.data)
+        return car
