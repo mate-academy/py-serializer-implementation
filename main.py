7cccbe1bf8f24794a8a7216a060bef46068389e0
sasha <magazynuniversam@gmail.com>
2024-01-26 00:17:08 +0200

    sol 5

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from rest_framework.utils import json
-
+import json
 from car.models import Car
 from car.serializers import CarSerializer
 
@@ -9,15 +8,18 @@ def serialize_car_object(car: Car) -> bytes:
     serialized_data = serializer.data
     json_str = json.dumps(serialized_data)
     serialized_bytes = json_str.encode("utf-8")
-
     return serialized_bytes
 
 
 def deserialize_car_object(json_data: bytes) -> Car:
-    json_file = json_data.decode("utf-8")
-    deserialized_data = json.loads(json_file)
-    serializer = CarSerializer(data=deserialized_data)
-    if serializer.is_valid(raise_exception=False):
-        return serializer.save()
-    else:
-        return None
+    try:
+        json_file = json_data.decode("utf-8")
+        deserialized_data = json.loads(json_file)
+        serializer = CarSerializer(data=deserialized_data)
+        if serializer.is_valid(raise_exception=True):
+            return serializer.save()
+    except json.JSONDecodeError as e:
+        print(f"JSON decoding error: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return None
