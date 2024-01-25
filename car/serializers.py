95abc29bad2d3ccb5df4d550027d251791d8a7f2
sasha <magazynuniversam@gmail.com>
2024-01-26 00:08:00 +0200

    sol 2

--- car/serializers.py
+++ car/serializers.py
@@ -11,22 +11,17 @@ class CarSerializer(serializers.Serializer):
         validators=[MaxValueValidator(1914), MinValueValidator(1)]
     )
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(null=True, blank=True)
+    problem_description = serializers.CharField(allow_null=True, allow_blank=True)
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         instance.model = validated_data.get("model", instance.model)
-        instance.horse_power = (validated_data.get
-                                ("horse_power", instance.horse_power))
-        instance.is_broken = (validated_data.get
-                              ("is_broken", instance.is_broken))
+        instance.horse_power = validated_data.get("horse_power", instance.horse_power)
+        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
         instance.problem_description = validated_data.get(
             "problem_description", instance.problem_description
         )
         instance.save()
         return instance
-
-    def delete(self, instance):
-        instance.delete()
