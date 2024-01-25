755b5a7dfd878b9840bd5ae913b73f0f7bb26883
sasha <magazynuniversam@gmail.com>
2024-01-26 00:10:21 +0200

    sol 4

--- car/serializers.py
+++ car/serializers.py
@@ -22,7 +22,8 @@ class CarSerializer(serializers.Serializer):
                           ("model", instance.model))
         instance.horse_power = (validated_data.get
                                 ("horse_power", instance.horse_power))
-        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
+        instance.is_broken = (validated_data.get
+                              ("is_broken", instance.is_broken))
         instance.problem_description = validated_data.get(
             "problem_description", instance.problem_description
         )
