d9fb0b56fc659853a04442f14b6c4b39a65030c5
Yuliana <pashkova.yuliana@gmail.com>
2024-02-19 16:38:27 +0200

    add serializer for car model & implement car serializer & deserializer

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,14 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from rest_framework import serializers
 
 
 class CarSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    manufacturer = serializers.CharField(required=True)
+    model = serializers.CharField(required=True)
+    horse_powers = serializers.IntegerField(required=True, validators=[
+        MaxValueValidator(1914),
+        MinValueValidator(1)
+    ])
+    is_broken = serializers.BooleanField(required=True)
+    problem_description = serializers.CharField(required=False, allow_blank=True)
--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+import io
+import json
+
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    data = JSONRenderer().render(serializer.data)
+    return data
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(json_data: bytes) -> Car:
+    stream = io.BytesIO(json_data)
+    data = JSONParser().parse(stream)
+    serializer = CarSerializer(data=data)
+    if serializer.is_valid():
+        return serializer.save()
