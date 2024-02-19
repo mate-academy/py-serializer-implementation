51cd23339d6b0b190941d38a0c9b002336a7e786
Yuliana <pashkova.yuliana@gmail.com>
2024-02-19 16:48:21 +0200

    fix errors

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from car.serializers import CarSerializer
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
     serialized_data = serializer.data
-    json_data = json.dumps(serialized_data).encode("utf-8")
+    json_data = json.dumps(serialized_data, sort_keys=True).encode("utf-8")
     return json_data
 
 
