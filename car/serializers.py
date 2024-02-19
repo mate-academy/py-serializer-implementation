cc65483934b0fdada894da180743c787f60ba850
Yuliana <pashkova.yuliana@gmail.com>
2024-02-19 16:42:14 +0200

    add create & update

--- car/serializers.py
+++ car/serializers.py
@@ -1,6 +1,8 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from rest_framework import serializers
 
+from car.models import Car
+
 
 class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
@@ -15,3 +17,25 @@ class CarSerializer(serializers.Serializer):
         required=False,
         allow_blank=True
     )
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.manufacturer = validated_data.get(
+            "manufacturer",
+            instance.manufacturer
+        )
+        instance.model = validated_data.get("model", instance.model)
+        instance.horse_powers = validated_data.get(
+            "horse_powers",
+            instance.horse_powers
+        )
+        instance.is_broken = validated_data.get(
+            "is_broken",
+            instance.is_broken
+        )
+        instance.problem_description = validated_data.get(
+            "problem_description",
+            instance.problem_description
+        )
