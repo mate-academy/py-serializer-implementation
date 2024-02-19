779dbb150d75c7bac993decf77656283c3f30e17
Yuliana <pashkova.yuliana@gmail.com>
2024-02-19 16:45:34 +0200

    fix errors

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-import io
+import json
 
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
 from car.models import Car
 from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
-    data = JSONRenderer().render(serializer.data)
-    return data
+    serialized_data = serializer.data
+    json_data = json.dumps(serialized_data).encode("utf-8")
+    return json_data
 
 
 def deserialize_car_object(json_data: bytes) -> Car:
-    stream = io.BytesIO(json_data)
-    data = JSONParser().parse(stream)
+    data = json.loads(json_data.decode("utf-8"))
     serializer = CarSerializer(data=data)
-    if serializer.is_valid():
-        return serializer.save()
+    serializer.is_valid(raise_exception=True)
+    return Car.objects.create(**serializer.validated_data)
