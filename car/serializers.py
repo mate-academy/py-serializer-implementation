73f0e788955da95b2f6adb87bcf80e9da35d3a81
3lobites <3lobites@gmail.com>
2024-02-21 15:43:14 +0200

    Start exercise

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,23 @@
 from rest_framework import serializers
 
+from car.models import Car
+
 
 class CarSerializer(serializers.Serializer):
-    pass
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField()
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(allow_null=True)
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.manufacturer = validated_data.get("manufacturer", instance.manufacturer)
+        instance.model = validated_data.get("model", instance.model)
+        instance.save()
+        return instance
+
+
+
