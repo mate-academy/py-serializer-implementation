5ffd60cc769fc12bf91942ae9b306805e402dbd9
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-02-25 19:07:29 +0200

    fix

--- car/views.py
+++ car/views.py
@@ -20,7 +20,8 @@
 #             serializer.save()
 #             return Response(serializer.data, status=status.HTTP_201_CREATED)
 #
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         return Response(serializer.errors,
+#         status=status.HTTP_400_BAD_REQUEST)
 #
 #
 # class CarDetail(APIView):
