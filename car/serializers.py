80fc8ae22798863ecce08db0730c2e6b56d38a5a
Molchanov Ihor <IMolchanov@kdmg.com.ua>
2024-03-27 18:17:44 +0200

    add annotation to func

--- car/serializers.py
+++ car/serializers.py
@@ -11,10 +11,10 @@ class CarSerializer(serializers.Serializer):
     is_broken = serializers.BooleanField(default=False)
     problem_description = serializers.CharField(required=False)
 
-    def create(self, validated_data):
+    def create(self, validated_data) -> Car:
         return Car.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self, instance, validated_data) -> Car:
         instance.manufacturer = validated_data.get(
             "manufacturer",
             instance.manufacturer
